03226db3b63e37ea0ff863d366405fbae2e9caab
Marcus Sanatan <msanatan@gmail.com>
2020-04-16 01:05:47 -0400

    Start electron in dev if testing locally
    I refused to install electron-is-dev package.
    We don't need packages for everything...
    
    Followed this answer to determine if we're in dev or not:
    https://github.com/electron/electron/issues/7714#issuecomment-310885608

---
+++ public/electron.js
@@ -0,0 +1,33 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const isDev = process.mainModule.filename.indexOf('app.asar') === -1 ||
+    process.mainModule.filename.indexOf('app') === -1;
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 1024, height: 768 });
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    if (isDev) {
+        // Open the DevTools.
+        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+        mainWindow.webContents.openDevTools();
+    }
+    mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
