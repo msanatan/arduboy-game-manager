5dd0ac2d73eba61b3bb062e9147bdf784a51505b
Marcus Sanatan <msanatan@gmail.com>
2020-04-17 21:05:11 -0400

    Upload a hex file to Arduboy
    The tricky bit was figuring out how to get Node modules.
    Docs said possible but not much examples over the internet.
    Guess that's my next blog post...

--- src/components/buttons/UploadButton.js
+++ src/components/buttons/UploadButton.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 
+const fs = window.require('fs').promises;
+const Avrgirl = window.require('avrgirl-arduino');
+const avrgirl = new Avrgirl({
+    board: 'leonardo',
+    debug: true
+});
+
+const flash = (...args) => {
+    return new Promise((resolve, reject) => {
+        avrgirl.flash(...args, (err) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        })
+    })
+}
+
 function slugify(label) {
     return label.toLowerCase().replace(/\s+/g, '-');
 }
@@ -19,21 +37,33 @@ class UploadButton extends Component {
         document.getElementById(this.state.inputId).click();
     }
 
+    uploadFile = async () => {
+        let filePath = document.getElementById(this.state.inputId).files[0].path;
+        console.log(filePath);
+        try {
+            let fileContents = await fs.readFile(filePath);
+            await flash(fileContents);
+        } catch (e) {
+            console.error(`could not upload Arduboy file: ${e}`);
+        }
+    }
+
     render() {
         return (
             <Button
-                variant="primary"
-                size="lg"
+                variant='primary'
+                size='lg'
                 block
                 onClick={this.openFileDialog}>
                 {`Upload ${this.state.label}`}
                 <input
                     hidden
-                    type="file"
+                    type='file'
                     id={this.state.inputId}
                     name={this.state.inputId}
                     accept={this.state.fileType}
-                    className="custom-file-input"/>
+                    onChange={this.uploadFile}
+                    className='custom-file-input' />
             </Button>
         );
     }
